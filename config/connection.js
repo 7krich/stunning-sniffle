1ad10fa690026bd547144001adf467ae74a55371
Kyle Richnafsky <7krich@gmail.com>
2022-05-15 09:50:38 -0400

    adjusgint server files

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,14 @@
-// import sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-// import dotenv variables from .env file
 require('dotenv').config();
 
-// create connection to jawsdb database
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    port: 3306
-  });
-}
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
