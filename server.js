92356c095b8f78386d17d045f00369699b7a4a41
Kyle Richnafsky <7krich@gmail.com>
2022-05-15 09:54:28 -0400

    reconfig & added missing.env link in server.js

--- server.js
+++ server.js
@@ -1,39 +1,44 @@
+// make css file available to client
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const routes = require('./controllers');
+// import connection from sequelize
+const sequelize = require('./config/connection');
+// set up handlebars.js
+const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require('dotenv').config();
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
   resave: false,
-  saveUninitialized: true,
+  saveUninitalized: true,
   store: new SequelizeStore({
     db: sequelize
   })
 };
 
 app.use(session(sess));
-
-const helpers = require('./utils/helpers');
-
-const hbs = exphbs.create({ helpers });
-
+// register hbs engine with the express app
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+// turn on routes
+app.use(routes);
 
+// turn on connection to db and server
+// set force: true so tables re-creat/ false to turn off
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
